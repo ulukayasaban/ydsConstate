52221a33b66e0da197b49233f1bbb0011afb8b18
Saban <ulukayasaban@gmail.com>
2020-02-25 22:52:58 +0300

    Initial Commit

---
+++ ydsKodTasarimi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ydsKodTasarimi
+//
+//  Created by Şaban ulukaya on 25.02.2020.
+//  Copyright © 2020 Şaban ulukaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
